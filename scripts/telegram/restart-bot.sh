9ae3c54eaeab9411312d6d265ac59cd85d39beff
Vladyslav Proshchavaiev <f3joule@gmail.com>
2021-11-22 12:36:34 +0200

    Add scripts to start, stop and restart telegram bot

---
+++ scripts/telegram/restart-bot.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+if [[ ! $(command -v pm2) ]]; then
+  echo -e "${RED}pm2 is not installed!"
+  echo -e "${NC}Run: yarn global add pm2"
+  exit 1
+fi
+
+pm2 restart subid-telegram-bot --update-env
